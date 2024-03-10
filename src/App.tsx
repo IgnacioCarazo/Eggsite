8f20bc145753fb655ce207bc0ac63fe94c546d02
Ignacio Carazo <ignaciocarazo@Ignacios-MacBook-Pro-2.local>
2024-03-10 15:56:38 -0600

    feat: add stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ReactElement, useState } from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import "./App.css";
 
-function App() {
+import { Window, WindowHeader, Button, styleReset, Toolbar, WindowContent, TextInput, AppBar,   } from 'react95';
+// pick a theme of your choice
+import original from 'react95/dist/themes/original';
+// original Windows95 font (optionally)
+import ms_sans_serif from 'react95/dist/fonts/ms_sans_serif.woff2';
+import ms_sans_serif_bold from 'react95/dist/fonts/ms_sans_serif_bold.woff2';
+
+const GlobalStyles = createGlobalStyle`
+  ${styleReset}
+  @font-face {
+    font-family: 'ms_sans_serif';
+    src: url('${ms_sans_serif}') format('woff2');
+    font-weight: 400;
+    font-style: normal
+  }
+  @font-face {
+    font-family: 'ms_sans_serif';
+    src: url('${ms_sans_serif_bold}') format('woff2');
+    font-weight: bold;
+    font-style: normal
+  }
+  body {
+    font-family: 'ms_sans_serif';
+  }
+`;
+
+export default function App(): ReactElement {
+
+  const [open, setOpen] = useState(false);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div className='background'>
+    <GlobalStyles />
+    <ThemeProvider theme={original}>
+    <Window className='window'>
+        <WindowHeader className='window-title'>
+          <span>eggs.exe</span>
+          <Button>
+            <span className='close-icon' />
+          </Button>
+        </WindowHeader>
+        <Toolbar>
+          <Button variant='menu' size='sm'>
+            File
+          </Button>
+          <Button variant='menu' size='sm'>
+            Edit
+          </Button>
+          <Button variant='menu' size='sm' disabled>
+            Save
+          </Button>
+        </Toolbar>
+        <WindowContent>
+          <p>
+            eggs eggs eggs!
+          </p>
+        </WindowContent>
+      </Window>
+      {/* <AppBar>
+        <Toolbar style={{ justifyContent: 'space-between'}}>
+          <div style={{ position: 'relative', display: 'inline-block' }}>
+            <Button 
+              onClick={() => setOpen(!open)}
+              active={open}
+              style={{ fontWeight: 'bold' }}
+            >
+              Start
+            </Button>
+            
+          </div>
+          <TextInput placeholder='Search...' width={150} />
+        </Toolbar>
+      </AppBar> */}
+    </ThemeProvider>
+  </div>)
 }
 
-export default App;
+
